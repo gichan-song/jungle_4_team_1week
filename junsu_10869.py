1b4ea2bcf3c074fe3a43318ef3a4809cc5283924
JunSu94 <90172003+JunSu94@users.noreply.github.com>
2023-03-03 16:26:12 +0900

    we should change way to soleve

---
+++ junsu_10869.py
@@ -0,0 +1,10 @@
+# input을 사용했지만 sys.stdin.readline()과 split을 이용하여야 할듯.
+# 열심히 고민했는데 오류가 계속뜸.
+A,B = input().split()
+A = int(A)
+B = int(B)
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)
