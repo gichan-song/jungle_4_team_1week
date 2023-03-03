810e83ab5551953116b44142c03a8deee1df7eb6
Gichani <thdrlcks97@gmail.com>
2023-03-03 21:21:11 +0900

    윤년

---
+++ 백준 문제풀이/백준 문제풀이/윤년.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void) {
+//
+//	int year = 0;
+//
+//	cin >> year;
+//
+//	if (year % 4 == 0 && year % 100 != 0) {
+//
+//		cout << 1 << endl;
+//
+//	}
+//
+//	else if (year % 400 == 0) {
+//
+//		cout << 1 << endl;
+//
+//	}
+//
+//	else {
+//
+//		cout << 0 << endl;
+//
+//	}
+//
+//	return 0;
+//}
