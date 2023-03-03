753e2b24b534367cf38de54f7c72efabd218c0ec
JunSu94 <90172003+JunSu94@users.noreply.github.com>
2023-03-03 16:47:37 +0900

    I am idiot

--- junsu_10869.py
+++ junsu_10869.py
@@ -1,10 +1,9 @@
-# input을 사용했지만 sys.stdin.readline()과 split을 이용하여야 할듯.
-# 열심히 고민했는데 오류가 계속뜸.
-A,B = input().split()
-A = int(A)
-B = int(B)
-print(A+B)
-print(A-B)
-print(A*B)
-print(int(A/B))
-print(A%B)
+import sys
+
+a,b = map(int, sys.stdin.readline().rstrip().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
+
